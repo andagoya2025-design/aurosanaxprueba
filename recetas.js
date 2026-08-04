5d16667a48f5b21cd331c625267ea59095ee7410
andagoya2025-design <andagoya2025@hotmail.com>
2026-08-04 15:37:13 -0500

    Update recetas.js

--- recetas.js
+++ recetas.js
@@ -2196,19 +2196,21 @@
           padding-top:5px!important;
           gap:12px!important;
           grid-template-columns:1.1fr .9fr!important;
-          align-items:end!important;
+          align-items:center!important;
           flex:0 0 auto!important;
         }
 
         .auro-media-receta .auro-centro-contacto{
           font-size:10.4px!important;
           line-height:1.10!important;
+          align-self:center!important;
         }
 
         .auro-media-receta .auro-firma{
-          padding-top:6px!important;
+          padding-top:0!important;
           font-size:10.8px!important;
           line-height:1.10!important;
+          align-self:center!important;
         }
 
         .auro-media-receta .auro-linea{
